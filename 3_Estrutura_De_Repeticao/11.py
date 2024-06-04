52a98b8ce885e84235ac4f8fef63f2d91df66ae1
estevaoaraujo <estevaorpa@gmail.com>
2024-06-04 19:13:54 -0300

    Estudando 4 junho 19:13

--- 3_Estrutura_De_Repeticao/11.py
+++ 3_Estrutura_De_Repeticao/11.py
@@ -1,1 +1,18 @@
+'''Faça um programa que receba dois números inteiros e gere os números inteiros que estão no intervalo compreendido por eles.'''
 #Altere o programa anterior para mostrar no final a soma dos números.
+
+numero1 = int(input("Número 1: "))
+numero2 = int(input("Número 2: "))
+resultado = 0
+
+while numero1 != numero2:
+    if numero1 > numero2:
+        numero2 += 1
+        resultado = resultado + numero2
+        print(numero2)
+    else:
+        numero1 < numero2    
+        numero1 += 1
+        resultado = resultado + numero1
+        print(numero1)                             #5 e 10 = 40
+print("Soma: ",resultado)
---
+++ 3_Estrutura_De_Repeticao/12.py
@@ -0,0 +1,23 @@
+'''Desenvolva um gerador de tabuada, capaz de gerar a tabuada de qualquer número inteiro entre 1 a 10. 
+O usuário deve informar de qual numero ele deseja ver a tabuada. A saída deve ser conforme o exemplo abaixo:
+Tabuada de 5:
+5 X 1 = 5
+5 X 2 = 10
+...
+5 X 10 = 50'''
+
+def entrada ():
+    tabuada_de = int(input("Tabuada de : "))
+    saida(tabuada_de)
+
+def saida(tabuada_de):
+    i = 0
+    while i <= 10:
+        if tabuada_de > 0 and tabuada_de <=10:
+            resutaldo = i * tabuada_de
+            print( i ," X ", tabuada_de," = ",resutaldo)
+            i+= 1
+        else:
+            print("Valor incorreto: ")    
+
+entrada()
